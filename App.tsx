05b99d22efe53452017fc845b84dc5198c8b60f2
jwAndroid <cjd9408er@naver.com>
2024-04-28 21:06:48 +0900

    update permission

--- App.tsx
+++ App.tsx
@@ -4,24 +4,36 @@
 
 import messaging from '@react-native-firebase/messaging';
 import React, {useEffect} from 'react';
-import {Alert} from 'react-native';
+import {Alert, PermissionsAndroid} from 'react-native';
 
 const App = () => {
-  const checkToken = async () => {
-    const token = await messaging().getToken();
+  useEffect(() => {
+    const requestUserPermission = async () => {
+      PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      );
 
-    if (token) {
-      console.log('token : ', token);
-    }
-  };
+      const authStatus = await messaging().requestPermission();
+      console.log('Authorization status:', authStatus);
+
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        const token = await messaging().getToken();
+        console.log('FCM token:', token);
+      }
+    };
+
+    requestUserPermission();
+  }, []);
 
   useEffect(() => {
     const unsubscribe = messaging().onMessage(async remoteMessage => {
       console.log(remoteMessage);
       Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
     });
-
-    checkToken();
 
     return unsubscribe;
   }, []);
